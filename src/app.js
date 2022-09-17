01f26fcfccab8162405296a3d14c783db55e547e
ElMrMel <40244617+ElMrMel@users.noreply.github.com>
2022-09-17 15:08:44 -0700

    Initilazied

---
+++ src/app.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", function(event) { 
+
+function navSlide() {
+    const burger = document.querySelector(".burger");
+    const nav = document.querySelector(".nav-links");
+    const navLinks = document.querySelectorAll(".nav-links li");
+    
+    burger.addEventListener("click", () => {
+        //Toggle Nav
+        nav.classList.toggle("nav-active");
+        document.body.classList.toggle('no-scroll')
+        //Animate Links
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = ""
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+            }
+        });
+        //Burger Animation
+        burger.classList.toggle("toggle");
+    });
+    
+}
+
+navSlide();
+
+});
+
